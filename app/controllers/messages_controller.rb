b7f257aa1dac14e6459459928fb12d83cf2ffdd3
jyoryu-0105 <ryutaro.nakanoue@gmail.com>
2020-10-06 18:09:06 +0900

    メッセージの表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,17 +3,20 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @room = Room.find(params[:room_id])
+    @messages = @room.messages.includes(:user)
+    # チャットルームのIDに紐づいた全てのメッセージ（@room.messages）を@messagesとして定義 / ユーザー情報も欲しいから取得（includes）
   end
 
   def create
     @room = Room.find(params[:room_id])
     # どのルームのお話なのかを特定(room_id)し変数に代入
     @message = @room.messages.new(message_params)
-    # チャットルームのIDに紐づいたメッセージのインスタンスを生成/どのユーザーがどんな内容のメッセージを送ったのかを受け取っている
+    # チャットルームのIDに紐づいたメッセージのインスタンスを生成 / どのユーザーがどんな内容のメッセージを送ったのかを受け取っている
     if @message.save
       redirect_to  room_messages_path(@room)
       # 参加しているチャットルームに投稿したメッセージの一覧画面
     else
+      @messages = @room.messages.include(:user)
       render :index
     end
   end
